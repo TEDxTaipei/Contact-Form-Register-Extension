1a13b347e73fb4bf8eb9d1b3187ebd91dc030959
蒼時弦也 <elct9620@frost.tw>
2013-07-11 11:59:34 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contact Form Register Extension
+
+這是 TEDxTaipei 為了解決活動報名問題而開發的 Plugin 透過 Contact Form 以及 Contact Form DB 的表單與資料記錄功能，額外擴充加入重複報名驗證以及通知訊息等新功能。
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Contact Form Registe Extension
+Plugin URI: https://github.com/TEDxTaipei/Contact-Form-Register-Extension
+Description: Let Contact From 7 can use for event register, and add send check mail for user and receive inbond mail when user reply mail.
+Author: TEDxTaipei
+Author URI: http://tedxtaipei.com/
+Text Domain: tedxtaipei-cfre
+Domain Path: /lang
+Version: 0.1.0
+*/
