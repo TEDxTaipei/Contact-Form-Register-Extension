4111a0d68de5cf5451017a7404eed617861423ca
蒼時弦也 <elct9620@frost.tw>
2013-07-12 11:51:55 +0800

    Update# add zh_TW language packate

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/TEDxTaipei/Contact-Form-Register-Extension
 Description: Let Contact From 7 can use for event register, and add send check mail for user and receive inbond mail when user reply mail.
 Author: TEDxTaipei
 Author URI: http://tedxtaipei.com/
-Text Domain: tedxtaipei-cfre
+Text Domain: tedxtaipei
 Domain Path: /lang
 Version: 0.1.0
 */
